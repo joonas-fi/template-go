cc46387cb011a1d5395ddaffdb3bfb1359829ca1
Joonas Loppi <joonas@joonas.fi>
2021-04-02 10:34:20 +0300

    add graceful exit template to main function

--- cmd/foo/main.go
+++ cmd/foo/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"context"
+
+	"github.com/function61/gokit/os/osutil"
+)
+
 func main() {
+	osutil.ExitIfError(
+		osutil.CancelOnInterruptOrTerminate(nil))
+}
+
+func logic(ctx context.Context)error{
+	return nil
 }
 
