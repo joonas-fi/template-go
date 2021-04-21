6919d2cdf24ac320049cf8f743f678041d0c4844
joonas.fi <pompo500@xs.fi>
2021-04-21 13:34:38 +0300

    add Cobra boilerplate

--- cmd/foo/main.go
+++ cmd/foo/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"context"
+	"os"
 
+	"github.com/function61/gokit/app/dynversion"
 	"github.com/function61/gokit/os/osutil"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	osutil.ExitIfError(
-		logic(osutil.CancelOnInterruptOrTerminate(nil)))
+	app := &cobra.Command{
+		Use: os.Args[0],
+		// Short:   "Description for app",
+		Version: dynversion.Version,
+	}
+
+	app.AddCommand(&cobra.Command{
+		Use: "reticulate-splines",
+		// Short: "Reticulates splines",
+		Args: cobra.NoArgs,
+		Run: func(_ *cobra.Command, args []string) {
+			osutil.ExitIfError(
+				logic(osutil.CancelOnInterruptOrTerminate(nil)))
+		},
+	})
+
+	osutil.ExitIfError(app.Execute())
 }
 
-func logic(ctx context.Context)error{
+func logic(ctx context.Context) error {
 	return nil
 }
-
