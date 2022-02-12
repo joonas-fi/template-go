e345b5632802081a29927c6951005d2d444a957d
Joonas Loppi <joonas+code@joonas.fi>
2022-02-12 19:25:15 +0200

    update boilerplates

--- bin/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-
-source /build-common.sh
-
-COMPILE_IN_DIRECTORY="cmd/foo"
-BINARY_NAME="foo"
-
-standardBuildProcess
--- bin/init-template.sh
+++ bin/init-template.sh
@@ -4,8 +4,6 @@ id="$1"
 
 mv cmd/foo "cmd/$id"
 
-sed -i "s/foo/$id/g" bin/build.sh
-
 sed -i "s/foo/$id/g" turbobob.json
 
 sed -i "s/template-go/$id/g" go.mod
@@ -17,7 +15,7 @@ rm -rf .git
 
 # replace template-repo specific readme (= it contains instructions related to the template itself)
 # with a blank one
-rm README.md && touch README.md
+rm README.md && echo -e "⬆️ For table of contents, click the above icon\n" > README.md
 
 # thank you for your service (pats self in the back)
 rm bin/init-template.sh
